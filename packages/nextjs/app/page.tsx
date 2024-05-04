9379aabab41fc87db2659995337180ed75218c9e
Syed Ghufran Hassan <syedghufranhassan@gmail.com>
2024-05-04 15:15:15 +0500

    Update page.tsx

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -5,7 +5,7 @@ import type { NextPage } from "next";
 import { useAccount } from "wagmi";
 import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { Address } from "~~/components/scaffold-eth";
-import { ethers } from "ethers"; 
+//import { ethers } from "ethers"; 
 
 const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
