400291d7376c340e59f1febbc03a79ffa8897faf
Syed Ghufran Hassan <syedghufranhassan@gmail.com>
2024-05-04 11:13:53 +0500

    Update page.tsx

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import type { NextPage } from "next";
-import { useAccount, useContractFunction, useContractCall, useScaffoldReadContract } from "wagmi";
+import { useAccount } from "wagmi";
 import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { Address } from "~~/components/scaffold-eth";
 
