94958216ed53dd2289b1369b4a7c40fc70c9d082
Syed Ghufran Hassan <syedghufranhassan@gmail.com>
2024-05-07 01:11:33 +0500

    Update page.tsx

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import Link from "next/link";
+//import Link from "next/link";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+//import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { Address } from "~~/components/scaffold-eth";
 import { ethers } from "ethers"; 
 
