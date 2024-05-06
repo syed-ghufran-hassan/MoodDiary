258e6299ce1a83283803b95d6cf6cd0cd5eb3ab1
Syed Ghufran Hassan <syedghufranhassan@gmail.com>
2024-05-07 01:00:07 +0500

    Update index.tsx

--- packages/nextjs/pages/debug/_components/contract/index.tsx
+++ packages/nextjs/pages/debug/_components/contract/index.tsx
@@ -1,8 +1,8 @@
-export * from "./ContractInput";
-export * from "./ContractUI";
-export * from "./DisplayVariable";
-export * from "./ReadOnlyFunctionForm";
-export * from "./TxReceipt";
-export * from "./utilsContract";
-export * from "./utilsDisplay";
-export * from "./WriteOnlyFunctionForm";
+export { default as ContractInput } from "./ContractInput";
+export { default as ContractUI } from "./ContractUI";
+export { default as DisplayVariable } from "./DisplayVariable";
+export { default as ReadOnlyFunctionForm } from "./ReadOnlyFunctionForm";
+export { default as TxReceipt } from "./TxReceipt";
+export { default as utilsContract } from "./utilsContract";
+export { default as utilsDisplay } from "./utilsDisplay";
+export { default as WriteOnlyFunctionForm } from "./WriteOnlyFunctionForm";
