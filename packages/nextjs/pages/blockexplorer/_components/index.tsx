3efe67c4c4716ca52c091f2deb58a429e010ab8b
Syed Ghufran Hassan <syedghufranhassan@gmail.com>
2024-05-07 00:57:43 +0500

    Update index.tsx

--- packages/nextjs/pages/blockexplorer/_components/index.tsx
+++ packages/nextjs/pages/blockexplorer/_components/index.tsx
@@ -1,7 +1,7 @@
-export * from "./SearchBar";
-export * from "./BackButton";
-export * from "./AddressCodeTab";
-export * from "./TransactionHash";
-export * from "./ContractTabs";
-export * from "./PaginationButton";
-export * from "./TransactionsTable";
+export { default as SearchBar } from "./SearchBar";
+export { default as BackButton } from "./BackButton";
+export { default as AddressCodeTab } from "./AddressCodeTab";
+export { default as TransactionHash } from "./TransactionHash";
+export { default as ContractTabs } from "./ContractTabs";
+export { default as PaginationButton } from "./PaginationButton";
+export { default as TransactionsTable } from "./TransactionsTable";
